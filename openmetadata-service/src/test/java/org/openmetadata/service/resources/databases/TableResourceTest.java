5f5027c97cdd5cc04d47648ad2128cb4d99b3164
Sriharsha Chintalapani <harsha@getcollate.io>
2023-04-13 16:34:13 -0700

    Fix styling

--- openmetadata-service/src/test/java/org/openmetadata/service/resources/databases/TableResourceTest.java
+++ openmetadata-service/src/test/java/org/openmetadata/service/resources/databases/TableResourceTest.java
@@ -1950,9 +1950,8 @@ public class TableResourceTest extends EntityResourceTest<Table, CreateTable> {
     return TestUtils.put(target, data, Table.class, OK, authHeaders);
   }
 
-
-  public static Table getTableProfileConfig(
-      UUID tableId, Map<String, String> authHeaders) throws HttpResponseException {
+  public static Table getTableProfileConfig(UUID tableId, Map<String, String> authHeaders)
+      throws HttpResponseException {
     WebTarget target = OpenMetadataApplicationTest.getResource("tables/" + tableId + "/tableProfilerConfig");
     return TestUtils.get(target, Table.class, authHeaders);
   }
